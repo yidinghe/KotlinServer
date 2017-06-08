02368a3feedd6293edae5bb1fe05a8c90ad712af
yiding he <yiding.he@samsungpay.com>
2017-06-08 17:27:21 -0400

    1. Added Client. 2. TODO JSON parse.

--- src/main/kotlin/StartPoint.kt
+++ src/main/kotlin/StartPoint.kt
@@ -1,8 +1,11 @@
+import com.github.kittinunf.fuel.httpGet
 import io.vertx.core.Vertx
+import io.vertx.core.http.HttpClientResponse
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.templ.ThymeleafTemplateEngine
 import verticles.MainVerticle
+import java.nio.charset.StandardCharsets
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -12,37 +15,52 @@ import java.util.*
 
 
 fun main(args: Array<String>) {
-//    val vertx = Vertx.vertx()
-//    vertx.deployVerticle(MainVerticle())
+    val vertx = Vertx.vertx()
 
-//    val vertx = Vertx.vertx()
-//
-//    val server = vertx.createHttpServer()
-//    val router = Router.router(vertx)
-//    val logger = LoggerFactory.getLogger("VertxServer")
-//    val templateEngine = ThymeleafTemplateEngine.create()
-//    val port = 10010
-//
-//    router.get("/home").handler {
-//        routingContext ->
-//        routingContext.put("time", SimpleDateFormat().format(Date()))
-//        templateEngine.render(routingContext, "public/templates/index.html", {
-//            buf ->
-//            if (buf.failed()) {
-//                logger.error("Template rendering failed", buf.cause())
-//            } else {
-//                val response = routingContext.response()
-//                response.end(buf.result())
-//            }
-//        })
-//    }
-//
-//    server.requestHandler { router.accept(it) }
-//            .listen(port, {
-//                listenHandler ->
-//                if (!listenHandler.succeeded()) {
-//                    logger.error("Failed to listen on port $port")
-//                }
-//            })
+    val server = vertx.createHttpServer()
+    val router = Router.router(vertx)
+    val logger = LoggerFactory.getLogger("VertxServer")
+    val templateEngine = ThymeleafTemplateEngine.create()
+    val port = 10004
+
+
+    if (TestConfig.IS_STOP_SERVER)
+        vertx.close()
+
+    val url = "http://api.sunrise-sunset.org/json?" +
+            "lat=42.3583333&lng=-71.0602778&formatted=0"
+
+
+
+    router.get("/home").handler {
+        routingContext ->
+
+        val (request, response, result) = url.httpGet().responseString()
+        val jsonStr = String(response.data, StandardCharsets.UTF_8)
+
+        println("jsonStr: $jsonStr")
+        //TODO jsonParse
+
+        routingContext.put("time", SimpleDateFormat().format(Date()) + "Yiding He")
+        templateEngine.render(routingContext, "public/templates/index.html", {
+            buf ->
+            if (buf.failed()) {
+                logger.error("Template rendering failed", buf.cause())
+            } else {
+                val responseOut = routingContext.response()
+                responseOut.end(buf.result())
+            }
+        })
+
+    }
+
+    server.requestHandler { router.accept(it) }
+            .listen(port, {
+                listenHandler ->
+                if (!listenHandler.succeeded()) {
+                    logger.error("Failed to listen on port $port")
+                }
+            })
+
 
 }
--- src/main/kotlin/verticles/MainVerticle.kt
+++ src/main/kotlin/verticles/MainVerticle.kt
@@ -1,11 +1,13 @@
 package verticles
 
+import com.github.kittinunf.fuel.httpGet
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.core.Vertx
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.templ.ThymeleafTemplateEngine
+import java.nio.charset.StandardCharsets
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -17,29 +19,41 @@ class MainVerticle : AbstractVerticle() {
 
     override fun start(startFuture: Future<Void>?) {
 
-
         val server = vertx.createHttpServer()
         val router = Router.router(vertx)
         val logger = LoggerFactory.getLogger("VertxServer")
         val templateEngine = ThymeleafTemplateEngine.create()
-        val port = 10011
+        val port = 8080
+
 
         if (TestConfig.IS_STOP_SERVER)
             vertx.close()
 
+        val url = "http://api.sunrise-sunset.org/json?" +
+                "lat=42.3583333&lng=-71.0602778&formatted=0"
+
+
+
         router.get("/home").handler {
             routingContext ->
-            routingContext.put("time", SimpleDateFormat().format(Date()))
+
+            val (request, response, result) = url.httpGet().responseString()
+            val jsonStr = String(response.data, StandardCharsets.UTF_8)
+
+            println("jsonStr: $jsonStr")
+            //TODO jsonParse
+
             routingContext.put("time", SimpleDateFormat().format(Date()) + "Yiding He")
             templateEngine.render(routingContext, "public/templates/index.html", {
                 buf ->
                 if (buf.failed()) {
                     logger.error("Template rendering failed", buf.cause())
                 } else {
-                    val response = routingContext.response()
-                    response.end(buf.result())
+                    val responseOut = routingContext.response()
+                    responseOut.end(buf.result())
                 }
             })
+
         }
 
         server.requestHandler { router.accept(it) }
@@ -49,6 +63,6 @@ class MainVerticle : AbstractVerticle() {
                         logger.error("Failed to listen on port $port")
                     }
                 })
-    }
 
+    }
 }
